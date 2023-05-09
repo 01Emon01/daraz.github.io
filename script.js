f646c345bf976f9d7c3d685bfe5796c33b271176
01Emon01 <133002138+01Emon01@users.noreply.github.com>
2023-05-09 16:31:36 +0600

    Daraz

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('.more-load').addEventListener("click", () =>{
+    document.querySelector('.jfu-button-func').style.display = "flex"
+    document.querySelector('.jfu-button-func2').style.display = "flex"
+})
